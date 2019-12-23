8a8f303fe86c78cdbb61b36850ac8e8f50e59517
TharushiDeSilva <tharushid.14@cse.mrt.ac.lk>
2019-12-23 18:36:28 +0530

    write inverse index including stop words

--- index_generator.cpp
+++ index_generator.cpp
@@ -12,9 +12,9 @@ using namespace std;
 static std::map<string, string> index_map; 
 static std::map<string, int> keyword_map;  
 
-const std::string index_file_name  ="ind_file.txt"; 
+const std::string index_file_name  ="index_file.txt"; 
 const std::string search_path = "search_space/url"; 
-const int dimesion = 1;                                 // number of URL files to read 
+const int dimesion = 3;                                 // number of URL files to read 
 
 std::string ignore_words[11] = {"the", "and", "that", "which", "are", "was", "were", "will", "then", "when", "what"}; 
 char ignore_chars[18] = {'&', '.', '>', '<', '?', '#', '@', '~', '!', '$', '%', '*', '(', ')', '[', ']', ',', ' '}; 
@@ -25,7 +25,9 @@ void add_word_to_local_index(std::string word){
     itr = keyword_map.find(word); 
     if(itr != keyword_map.end()){
                                                             // if the word is found previously in the map 
-        itr->second = itr->second+1;                        // increse the number of occurances 
+        int count = itr->second; 
+        count +=1;                         // increse the number of occurances 
+        itr->second = count; 
     }else{
         keyword_map.insert(std::pair<string, int>(word, 1));    // first time the word is found in the file 
     }
@@ -40,19 +42,24 @@ void update_global_index(std::string url){                      // adding the gl
     std::map<std::string, int>::iterator itr_local;
     for(itr_local = keyword_map.begin(); itr_local != keyword_map.end(); itr_local++){
         std::string pre_keyword = itr_local->first; 
-        for(int k=0; k < 11; k++){
-            if(pre_keyword ==ignore_words[k]){
-                continue;                                       //stop word detected
-            }else{
-                std::map<std::string, string>::iterator itr; 
-                itr = index_map.find(pre_keyword); 
-                if(itr != index_map.end()){                     //if the keyword exists
-                    itr->second = itr->second + ", "+ url; 
-                }else{
-                    index_map.insert(std::pair<string, string>(pre_keyword, url));
-                }
-            }  
-        }        
+        // for(int k=0; k < 11; k++){
+        //     if(pre_keyword == ignore_words[k]){
+        //         std::cout<<"break word detected: "<<pre_keyword<<std::endl; 
+        //         break;                                       //stop word detected
+        //     }
+        // }
+        
+        std::map<std::string, string>::iterator itr; 
+        itr = index_map.find(pre_keyword); 
+        
+        if(itr != index_map.end()){                     //if the keyword exists
+            std::string url_list = itr->second; 
+            url_list += ", " + url; 
+            itr->second = url_list; 
+        }else{
+            index_map.insert(std::pair<string, string>(pre_keyword, url));
+        }
+                      
     }
 }
 
@@ -76,20 +83,21 @@ void generate_inverse_index(){
 
                 if(firstline){
                     url = line;                                                             // assign the firsline value to url 
+                    url.erase(url.find_last_not_of(" \n\r\t")+1);
                     firstline = false;  
                 }
                 else{
                     // content of the webpage 
                     boost::to_lower(line);                      // convert the whole line into lowercase 
-                    line.erase(0, line.find_first_not_of("& \'\"\t.><?#@~!$%*()[],"));
-                    line.erase(line.find_last_not_of("& \'\"\t.><?#@~!$%*()[],"));
+                    line.erase(0, line.find_first_not_of("-& \'\"\t.><?#@~!$%*()[],"));
+                    line.erase(line.find_last_not_of("-& \'\"\t.><?#@~!$%*()[],"));
                     
                     // Seperate word by word 
                     std::string word = ""; 
                     for (int i=0; i < line.length( ); i++){
                         for(int j=0; j<sizeof(ignore_chars); j++){
                             if(line[i] == ignore_chars[j]){
-                                word.erase(0, word.find_first_not_of("& \'\"\t.><?#@~!$%*()[],"));
+                                word.erase(0, word.find_first_not_of("-& \'\"\t.><?#@~!$%*()[],"));
                                 if(word.length() >2){                                                   // can remove simple stop words, and empty lines
                                     add_word_to_local_index(word); 
                                 }
@@ -100,7 +108,7 @@ void generate_inverse_index(){
                         }
                         word += line[i];  
                     } 
-                    word.erase(0, word.find_first_not_of("& \'\"\t.><?#@~!$%*()[],"));
+                    word.erase(0, word.find_first_not_of("-& \'\"\t.><?#@~!$%*()[],"));
                     if(word.length() >2){                                                           // can remove simple stop words, and empty lines
                         add_word_to_local_index(word); 
                     }
@@ -117,7 +125,15 @@ void generate_inverse_index(){
 }
 
 void write_reverse_index_to_file(std::string filename){                 //write the index_map into a text file for reference 
-    
+      std::ofstream ofile;
+        ofile.open (filename.c_str(), ios::app);  
+        if(ofile.is_open()) {
+            std::map<std::string, string>::iterator itr;
+            for(itr = index_map.begin(); itr != index_map.end(); itr++){
+                ofile<<itr->first<<"\t{"<<itr->second<<"}"<<endl; 
+            }
+        }
+        ofile.close(); 
 }
 
 int main(){
